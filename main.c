5cff86f1b1e485fe10034eff80cd078c9077d1d9
lizorazi <lizorazi@users.noreply.github.com>
2021-01-16 16:12:11 +0100

    The way generator works has been updated. You need to specify freq and amp before starting DAC.

--- main.c
+++ main.c
@@ -6,10 +6,7 @@
 				Creators:
 				Przemyslaw Grobecki
 				Justyna Malys
-				
-				If you want to show the current frequency level on the LCD- uncomment all the necessery lines.
-****************************************************************************************************************************/
-
+*****************************************************************************************************************************/
 
 /*** Necessery Includes ***/
 #include "MKL05Z4.h"
@@ -21,44 +18,36 @@
 #include "tsi.h"
 #include "buttons.h"
 #include "lcd1602.h"
-#include "pit.h"
+
+#define NUMBER_OF_SIGNAL_SAMPLES 20
+#define FREQUENCY_COEFFICIENT 1200
+
+typedef enum Mode {Amplitude_Adjustion, Frequency_Adjustion, Signal_Go} Mode;
+typedef enum Signal {Signal_Sinus, Signal_Saw, Signal_Triangle} Signal;
 
 /*** Necessery Global Variables ***/
 double beep = 0;														//resambles a single tone provided by for example a buzzer connected to the generator
+double amplitude = 0.0;
+double frequency = 0.0; 
+uint16_t amplitude_modifier = 1;
 uint8_t wynik_ok = 1; 											//used to comunicate interrupts with the main loop
-uint8_t PIT_enable = 0;										//used in frequency_show mode to enable PIT_interrupts mode
-static uint8_t w1 = 1; 											//used to get the the value from slider
-static uint8_t w2 = 1; 											//used in frequency_show mode
-double freq = {0.0}; 
+uint8_t show_signal = 1;									
+uint8_t w1 = 1; 														//used to get the the value from slider						
 double Sinus[100] = {0.0}; 									//Sinus vector where the sinus values will be stored
 double Saw[100] = {0.0};	 									//Saw vector where the saw values will be stored
 double Triangle[100] = {0.0};								//Triangle vector where the triangle values will be stored
 int counter = 0;
 char display[]={0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20};
 
-static unsigned char generator_mode = 0;
+static unsigned char chosen_signal = 0;
+static unsigned char chosen_mode = Frequency_Adjustion;
 
 /* Function that change signel type */
 void Change_Signal(void);
+void Change_Mode(void);
 
 /* In this handler we push the values of given signal to the DAC */
 void SysTick_Handler(void);
-
-
-void PIT_IRQHandler()
-{		
-	PIT_enable = 1;
-	freq = 1.0/(2000*w2);
-	//freq *= 0.00000006;
-	freq = 100*freq;
-	freq = 1.0/freq;
-	sprintf(display,"Freq: %.1lf",freq);
-	LCD1602_SetCursor(0,0);
-	LCD1602_Print(display);	
-	PIT_enable = 0;
-	PIT->CHANNEL[0].TFLG = PIT_TFLG_TIF_MASK;		// Skasuj flage zadania przerwania
-}
-
 
 /* SysTick initialisation function */
 void SysTick_Init(unsigned int divider);
@@ -74,7 +63,7 @@ void TriangleInit(double *triangle);
 
 int main (void){
 	/* Init block */
-	SysTick_Init(16777216);
+	SysTick_Init(SystemCoreClock/1000);
 	buttonsInitialize();
 	SineInit(Sinus);
 	SawInit(Saw);
@@ -83,23 +72,54 @@ int main (void){
 	LCD1602_Backlight(TRUE);
 	TSI_Init();
 	DAC_Init();
-	PIT_Init();
+
 	/* Init block - End */
 	
 	DAC_Load_Trig(0);	// Wyzwolenie prztwornika C/A wartoscia poczatkowa
 	while(1){
-	if(!PIT_enable){
+		
+		if(!(chosen_mode == Signal_Go)){
 			w1 = TSI_ReadSlider();	
-			if(w1!=0){
-				w2=w1;
-				SysTick_Config( 16777216/ (w2*2000)); //By changing systick options we modulate the frequency SystemCoreClock
-			}
-			if(wynik_ok){
-				DAC_Load_Trig((uint16_t)(2000*beep));
-				wynik_ok = 0; 
-			}
 		}
- }
+		
+		if(chosen_mode==Frequency_Adjustion){
+			if(w1!=0){
+				SysTick_Config((uint32_t)(SystemCoreClock / (w1*FREQUENCY_COEFFICIENT))); //By changing systick options we modulate the frequency
+				frequency = (FREQUENCY_COEFFICIENT*w1)/(double)NUMBER_OF_SIGNAL_SAMPLES;
+				sprintf(display,"Freq: %.0lf Hz", frequency);
+				LCD1602_SetCursor(0,0);
+				LCD1602_Print(display);
+			}
+		}	
+		
+		if(chosen_mode==Amplitude_Adjustion){
+			if(w1!=0) amplitude_modifier = w1;
+			amplitude = amplitude_modifier*20*2.91/4095;
+			sprintf(display,"Amp: %.3lf V", amplitude);
+			LCD1602_SetCursor(0,1);
+			LCD1602_Print(display);
+		}
+		
+		if(show_signal){
+			LCD1602_SetCursor(13,1);
+			if(chosen_signal == Signal_Sinus){
+				LCD1602_Print("SIN");
+			}
+			if(chosen_signal == Signal_Saw){
+				LCD1602_Print("SAW");
+			}
+			if(chosen_signal == Signal_Triangle){
+				LCD1602_Print("TRI");
+			}
+			show_signal = 0;
+		}
+		if(chosen_mode == Signal_Go){
+			if(wynik_ok){
+				DAC_Load_Trig((uint16_t)(20*amplitude_modifier*beep));
+				wynik_ok = 0; 
+			}	
+		}
+	}
 }
 
 
@@ -111,37 +131,51 @@ int main (void){
 
 /* Function that change signel type */
 void Change_Signal(void){
-	generator_mode+=1;
-	counter =0;
-	if(generator_mode>2){
-		generator_mode =0;
+	if(~show_signal){
+		chosen_signal+=1;
+		counter =0;
+		if(chosen_signal>2){
+			chosen_signal =0;
+		}
+		show_signal = 1;
+	}
+	
+}
+
+void Change_Mode(void){
+	if(chosen_mode == Frequency_Adjustion){
+		chosen_mode = Amplitude_Adjustion;
+	}
+	else if (chosen_mode == Amplitude_Adjustion){
+		chosen_mode = Signal_Go;
+	}
+	else if (chosen_mode == Signal_Go){
+		chosen_mode = Frequency_Adjustion;
 	}
 }
 
 /* In this handler we set the values of given signal to the beep so the DAC can use them */
 void SysTick_Handler(void){
-if(!PIT_enable){
-		if(~wynik_ok){
-			if(generator_mode == 0){
-				beep = Sinus[counter];
-				counter++;
-				if(counter == 99) counter = 0;
-				wynik_ok =1;
-			}
-			else if(generator_mode==1){
-				beep = Saw[counter];
-				counter++;
-				if(counter == 99) counter = 0;
-					wynik_ok =1;
-			}
-			else{
-				beep = Triangle[counter];
-				counter++;
-				if(counter == 99) counter = 0;
-				wynik_ok =1;
-			}
+	if(~wynik_ok){
+		if(chosen_signal == 0){
+			beep = Sinus[counter];	
+			counter++;
+			if(counter == NUMBER_OF_SIGNAL_SAMPLES-1) counter = 0;
+			wynik_ok =1;
 		}
-}
+		else if(chosen_signal==1){
+			beep = Saw[counter];
+			counter++;
+			if(counter == NUMBER_OF_SIGNAL_SAMPLES-1) counter = 0;
+				wynik_ok =1;
+		}
+		else{
+			beep = Triangle[counter];
+			counter++;
+			if(counter == NUMBER_OF_SIGNAL_SAMPLES-1) counter = 0;
+			wynik_ok =1;
+		}
+	}
 }
 
 
@@ -152,25 +186,25 @@ void SysTick_Init(unsigned int divider){
 
 /* Sinus vector filler */
 void SineInit(double *sinus){
-	for(int i = 0 ; i < 100 ; i++){
-		sinus[i] =1 + sin((double)(i * 3.14 *2/100));
+	for(int i = 0 ; i < NUMBER_OF_SIGNAL_SAMPLES ; i++){
+		sinus[i] =1 + sin((double)(i * 3.14 *2/NUMBER_OF_SIGNAL_SAMPLES));
 	}
 }
 
 /* Saw vector filler */
 void SawInit(double *saw){
-	for(int i = 0; i<100; i++){
-		saw[i] = (double)(i) * 2/100;
+	for(int i = 0; i<NUMBER_OF_SIGNAL_SAMPLES; i++){
+		saw[i] = (double)(i) * 2/NUMBER_OF_SIGNAL_SAMPLES;
 	}
 }
 
 /* Triangle vector filler */
 void TriangleInit(double *triangle){
-	for(int i = 0; i< 50 ; i++){
-		triangle[i] = (double)(i) * 2/50;
+	for(int i = 0; i< NUMBER_OF_SIGNAL_SAMPLES/2 ; i++){
+		triangle[i] = (double)(i) * 2/(NUMBER_OF_SIGNAL_SAMPLES/2);
 	}
-	for(int i = 50; i < 100; i++){
-		triangle[i] = (double)(100-i)*2/50;
+	for(int i = NUMBER_OF_SIGNAL_SAMPLES/2; i < NUMBER_OF_SIGNAL_SAMPLES; i++){
+		triangle[i] = (double)(NUMBER_OF_SIGNAL_SAMPLES-i)*2/(NUMBER_OF_SIGNAL_SAMPLES/2);
 	}
 }
 
